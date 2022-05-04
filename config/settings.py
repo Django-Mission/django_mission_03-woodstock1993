176a480024e34280257160146525b1671d8d6159
woodstock1993 <diqkqkqk@gmail.com>
2022-05-05 02:29:35 +0900

    add: inquiry list 반환 view

--- config/settings.py
+++ config/settings.py
@@ -9,24 +9,30 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
-from pathlib import Path
+from email.charset import BASE64
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import environ
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-import environ
 
-env = environ.Env(DEBUG=(bool, True))
-environ.ENV.read_env(
-    env_file=os.path(BASE_DIR, '.env')
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, True)
+)
+
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
 )
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- support/views.py
+++ support/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Inquiry
+from .serializers import InquirySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
-class InquiryViewset():
-    pass
+class InquiryList(APIView):
+    def get(self, request):
+        inquiries = Inquiry.objects.all()
+        serializer = InquirySerializer(inquiries, many=True)
+        print(serializer)
+        return Response(serializer.data)
+
+
